c5f730648dc83b9179d029a08f45653a5722e91f
saswatjit ptra <saswatp813@gmail.com>
2025-09-16 18:09:01 +0530

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://lead.indibus.net"],
+    origin: ["http://localhost:3000", "https://lead-managment-f.vercel.app/"],
     credentials: true,
   })
 );
