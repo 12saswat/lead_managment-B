456a909a55dc423bc23c34d96ed24247d90284e0
saswatjit ptra <saswatp813@gmail.com>
2025-09-16 17:53:24 +0530

    fix- cors

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://lead.indibus.net",
+    origin: ["http://localhost:3000", "https://lead.indibus.net"],
     credentials: true,
   })
 );
